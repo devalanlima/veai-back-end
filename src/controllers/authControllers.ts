4b3cf2e0882ae72844d74c54fcfe110a0ed8c5b9
Alan Lima <dev.alanlima@gmail.com>
2024-12-04 15:49:42 -0300

    feat: create new authenticate user auth controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,6 +1,6 @@
-import { signUpNewUser } from '../services/authServices';
+import { signInUser, signUpNewUser } from '../services/authServices';
 import { Request, Response } from 'express';
-import { SignUpBody } from '../types/authTypes';
+import { SignInBody, SignUpBody } from '../types/authTypes';
 
 export async function createNewUser(req: Request, res: Response) {
   const { email, password, name } = req.body as SignUpBody;
@@ -10,5 +10,16 @@ export async function createNewUser(req: Request, res: Response) {
     res.send({ msg: error.message }).status(error.status ? error.status : 400);
   } else {
     res.send(data).status(201);
+  }
+}
+
+export async function authenticateUser(req: Request, res: Response) {
+  const { email, password } = req.body as SignInBody;
+  const { data, error } = await signInUser(email, password);
+
+  if (error) {
+    res.send({ msg: error.message }).status(error.status ? error.status : 400);
+  } else {
+    res.send(data).status(200);
   }
 }
