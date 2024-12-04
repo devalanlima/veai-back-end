8da8215f64fc08b8ba071eb32b0cf4dc764285e1
Alan Lima <dev.alanlima@gmail.com>
2024-12-04 15:11:06 -0300

    feat: create app to initiate express and use routes

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Express } from "express";
+import authRoutes from "../src/routes/authRoutes";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes)
+
+export default app
