2ce1a73ea8c3258602ce47747543b556e7be13c2
Alan Lima <dev.alanlima@gmail.com>
2024-12-06 12:35:07 -0300

    feat: create forgot password route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   authenticateUser,
   createNewUser,
+  sendRecoveryPasswordEmail,
 } from '../controllers/authControllers';
 
 const router = express.Router();
 
 router.post('/signup', createNewUser);
 router.post('/signin', authenticateUser);
+router.post('/forgot-password', sendRecoveryPasswordEmail);
 
 export default router;
