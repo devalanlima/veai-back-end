93416bea6fb4e4398ff9ea0a332919150afcf8ff
Alan Lima <dev.alanlima@gmail.com>
2024-12-06 12:34:09 -0300

    feat: create reset password auth controller

--- src/controllers/authControllers.ts
+++ src/controllers/authControllers.ts
@@ -1,6 +1,10 @@
-import { signInUser, signUpNewUser } from '../services/authServices';
+import {
+  resetPassword,
+  signInUser,
+  signUpNewUser,
+} from '../services/authServices';
 import { Request, Response } from 'express';
-import { SignInBody, SignUpBody } from '../types/authTypes';
+import { ResetPasswordBody, SignInBody, SignUpBody } from '../types/authTypes';
 
 export async function createNewUser(req: Request, res: Response) {
   const { email, password, name } = req.body as SignUpBody;
@@ -21,5 +25,18 @@ export async function authenticateUser(req: Request, res: Response) {
     res.send({ msg: error.message }).status(error.status ? error.status : 400);
   } else {
     res.send(data).status(200);
+  }
+}
+
+export async function sendRecoveryPasswordEmail(req: Request, res: Response) {
+  const { email } = req.body as ResetPasswordBody;
+  const { error } = await resetPassword(email);
+
+  if (error) {
+    res.send({ msg: error.message }).status(error.status ? error.status : 400);
+  } else {
+    res
+      .send({ msg: 'Password reset request sent to email address' })
+      .status(200);
   }
 }
