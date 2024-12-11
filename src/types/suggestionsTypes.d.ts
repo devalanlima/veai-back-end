0e0288b7909c8dc7ca1a831decdea645ed77b3c0
Alan Lima <dev.alanlima@gmail.com>
2024-12-11 12:35:48 -0300

    feat: create new suggestions types for query params

---
+++ src/types/suggestionsTypes.d.ts
@@ -0,0 +1,10 @@
+import { Tables } from './supabase';
+
+export interface SuggestionsQuery {
+  pageSize?: number;
+  page?: number;
+  order?: keyof Tables<'Suggestions'>;
+  ascending?: boolean;
+  movieId?: string;
+  genres?: number[];
+}
