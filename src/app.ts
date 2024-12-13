6813fac2b42b7bec74f23380ea2905ebd28f8221
Alan Lima <dev.alanlima@gmail.com>
2024-12-13 12:46:29 -0300

    feat: add new suggestions and genres routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Express } from 'express';
 import authRoutes from '../src/routes/authRoutes';
+import suggestionsRoutes from '../src/routes/suggestionsRoutes';
+import genresRoutes from './routes/genresRoutes';
 
 const app: Express = express();
 
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api', suggestionsRoutes);
+app.use('/api', genresRoutes);
 
 export default app;
