bd5cd525ae3e5d67e4f3787acd036ce013d3c2f9
Alan Lima <dev.alanlima@gmail.com>
2024-12-13 12:43:03 -0300

    feat: create genres controller

---
+++ src/controllers/genresControllers.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { z } from 'zod';
+import { filterGenres } from '../services/genresServices';
+import { GenresQuery, GenresQuerySchema } from '../schemas/genresSchemas';
+
+export async function getGenres(req: Request, res: Response) {
+  let query: GenresQuery;
+
+  try {
+    query = GenresQuerySchema.parse(req.query);
+  } catch (error) {
+    res.status(400).json({
+      msg: 'Invalid query parameters',
+      errors: error instanceof z.ZodError ? error.issues : 'Validation error',
+    });
+    return;
+  }
+
+  const { data: suggestions, error, status } = await filterGenres(query);
+
+  if (suggestions?.length === 0) {
+    res.status(200).json({
+      msg: 'No items found',
+      data: suggestions,
+    });
+    return;
+  }
+
+  if (error) {
+    res.status(status).json({ msg: error.message });
+    return;
+  } else {
+    res.status(status).json(suggestions);
+    return;
+  }
+}
