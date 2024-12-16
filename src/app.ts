2c073a82ec29ed82f84c8c2395b87fc94c9fbc36
Alan Lima <dev.alanlima@gmail.com>
2024-12-16 12:54:34 -0300

    feat: add tmdb route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import authRoutes from '../src/routes/authRoutes';
 import suggestionsRoutes from '../src/routes/suggestionsRoutes';
 import genresRoutes from './routes/genresRoutes';
+import tmdbRoutes from './routes/tmdbRoutes';
 
 const app: Express = express();
 
@@ -10,5 +11,6 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api', suggestionsRoutes);
 app.use('/api', genresRoutes);
+app.use('/api/tmdb', tmdbRoutes);
 
 export default app;
