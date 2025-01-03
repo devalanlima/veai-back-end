b3dbd7cc1e97f6ac960b12f3422da2b6d0639a14
Alan Lima <dev.alanlima@gmail.com>
2025-01-03 12:27:12 -0300

    feat: create new controller to insert watched

--- src/controllers/watchedControllers.ts
+++ src/controllers/watchedControllers.ts
@@ -1,7 +1,13 @@
-import { WatchedQuery, WatchedQuerySchema } from '../schemas/watchedSchemas';
-import { filterWatched } from '../services/watchedServices';
+import {
+  InsertWatched,
+  InsertWatchedSchema,
+  WatchedQuery,
+  WatchedQuerySchema,
+} from '../schemas/watchedSchemas';
+import { filterWatched, insertNewWatched } from '../services/watchedServices';
 import { z } from 'zod';
 import { Request, Response } from 'express';
+import { TablesInsert } from '../types/supabase';
 
 export async function getWatched(req: Request, res: Response) {
   let query: WatchedQuery;
@@ -33,4 +39,30 @@ export async function getWatched(req: Request, res: Response) {
     res.status(status).json(watched);
     return;
   }
+}
+
+export async function insertWatched(
+  req: Request<{}, {}, TablesInsert<'Watched'>>,
+  res: Response,
+) {
+  let body: InsertWatched;
+
+  try {
+    body = InsertWatchedSchema.parse(req.body);
+  } catch (error) {
+    res.status(400).json({
+      msg: 'Invalid body parameters',
+      errors: error instanceof z.ZodError ? error.issues : 'Validation error',
+    });
+    return;
+  }
+
+  const { data, error, status } = await insertNewWatched(body);
+
+  if (error) {
+    res.json(error).status(status);
+    return;
+  }
+
+  res.json(data).status(status);
 }
