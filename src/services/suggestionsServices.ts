0c82813d3cde05b838560395b2e95bd0e857fb5b
Alan Lima <dev.alanlima@gmail.com>
2025-01-14 12:02:20 -0300

    feat: create new service to insert new suggestion

--- src/services/suggestionsServices.ts
+++ src/services/suggestionsServices.ts
@@ -1,4 +1,5 @@
 import { SuggestionsQuery } from '../schemas/suggestionsSchemas';
+import { TablesInsert } from '../types/supabase';
 import supabase from '../utils/supabaseClient';
 
 export async function filterSuggestions({
@@ -19,6 +20,33 @@ export async function filterSuggestions({
   genres && query.contains('genres', genres);
 
   const data = await query;
+
+  return data;
+}
+
+export async function insertNewSuggestion({
+  movie_id,
+  sender_id,
+  created_at,
+  genres,
+  receiver_id,
+  spoiler,
+  suggestion,
+  vote_count,
+}: TablesInsert<'Suggestions'>) {
+  const data = await supabase
+    .from('Suggestions')
+    .insert({
+      movie_id,
+      sender_id,
+      created_at,
+      genres,
+      receiver_id,
+      spoiler,
+      suggestion,
+      vote_count,
+    })
+    .select();
 
   return data;
 }
