1fcb56908a639b2800e2ba2860f51a04d0e4847b
Alan Lima <dev.alanlima@gmail.com>
2025-01-14 11:12:15 -0300

    feat: create new service to insert movie in watchlist

--- src/services/watchlistServices.ts
+++ src/services/watchlistServices.ts
@@ -1,4 +1,5 @@
 import { WatchlistQuery } from '../schemas/watchlistSchemas';
+import { TablesInsert } from '../types/supabase';
 import supabase from '../utils/supabaseClient';
 
 export async function filterWatchlist({
@@ -20,6 +21,27 @@ export async function filterWatchlist({
   genres && query.contains('genres', genres);
 
   const data = await query;
+
+  return data;
+}
+
+export async function insertInWatchlist({
+  created_at,
+  genres,
+  movie_id,
+  user_id,
+}: TablesInsert<'Watchlist'>) {
+  const data = await supabase
+    .from('Watchlist')
+    .insert([
+      {
+        movie_id,
+        user_id,
+        created_at,
+        genres,
+      },
+    ])
+    .select();
 
   return data;
 }
