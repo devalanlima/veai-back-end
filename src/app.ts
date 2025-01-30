cb5758860d62f5395c587f201468247ffcb3f1e4
Alan Lima <dev.alanlima@gmail.com>
2025-01-30 13:35:12 -0300

    feat: Add cors police

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,12 @@ import genresRoutes from './routes/genresRoutes';
 import tmdbRoutes from './routes/tmdbRoutes';
 import watchedRoutes from './routes/watchedRoutes';
 import watchlistRoutes from './routes/watchlistRoutes';
+import cors from 'cors';
 
 const app: Express = express();
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api', suggestionsRoutes);
